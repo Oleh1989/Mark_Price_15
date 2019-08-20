b046bd4a2f8b20855bc6ed388996d6618ff3c5ad
Олег Бабков <babkoff1989@gmail.com>
2019-08-20 18:47:15 +0300

    Added Core MVC and Web projects

---
+++ NorthwindEntitiesLib/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthwindEntitiesLib
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public DateTime? RequiredDate { get; set; }
+        public DateTime? ShippedDate { get; set; }
+        public int ShipVia { get; set; }
+        public Shipper Shipper { get; set; }
+        public decimal? Freight { get; set; }
+        public ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ NorthwindEntitiesLib/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthwindEntitiesLib
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int? SupplierId { get; set; }
+        public Supplier Supplier { get; set; }
+        public int? CategoryId { get; set; }
+        public Category Category { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal? UnitPrice { get; set; } = 0;
+        public short? UnitsInStock { get; set; } = 0;
+        public short? UnitsInOrder { get; set; } = 0;
+        public short? ReorderLevel { get; set; } = 0;
+        public bool Discounted { get; set; } = false;
+    }
+}
